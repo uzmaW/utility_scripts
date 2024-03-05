0e0c617393b7bcd89ce3b0219ef5379782e4c27b
Uzma <nasiruddin.uzma@gmail.com>
2024-03-05 20:42:57 +0500

    replace file name with given string

--- renamefiles.sh
+++ renamefiles.sh
@@ -1,10 +1,8 @@
 str=$1;
 strRep=$2;
 
-find . -type f -name "*${str}*" | while read FILE ; do
-    
-    newfile="$(echo ${FILE} |sed -e 's/"+"${str}/${strRep}"+"/')" ;
-    #mv "${FILE}" "${newfile}" ;
-    echo "${newfile}";
-    #echo "${str} ${strRep}";
-done 
+find . -type f -name "*${str}*" | while read FILE ;do 
+    newfile="$(echo ${FILE} | sed -e s/${str}/${strRep}/)" ; 
+    echo "${newfile}"; 
+    mv "${FILE}" "${newfile}"; 
+done; 
